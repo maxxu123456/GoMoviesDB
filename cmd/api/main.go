01e7459a33a7b69a3b39aa6dd003d714f5b46975
Max Xu <maxxu123456@gmail.com>
2025-05-23 15:51:15 -0400

    added basic Home Handler

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,4 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port = 8080
+
+type application struct {
+	Domain string
+}
+
 func main() {
+
+	var app application
+
+	app.Domain = "example.com"
+
+	log.Println("Starting application on port:", port)
+
+	//start web server
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
+	if err != nil {
+		log.Fatal(err)
+	}
 }
