3b309d903fb602829e778857b2f13cc0f6b60618
Max Xu <maxxu123456@gmail.com>
2025-05-24 20:23:06 -0400

    added basic go integration with postgres

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,19 +11,31 @@ import (
 const port = 8080
 
 type application struct {
+	DSN    string
 	Domain string
+	DB     *sql.DB
 }
 
 func main() {
 
 	var app application
 
+	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres dbname=movies sslmode=disable timezone=UTC connect_timeout=5", "Postgres connection string")
+	flag.Parse()
+
+	conn, err := app.connectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	app.DB = conn
+
 	app.Domain = "example.com"
 
 	log.Println("Starting application on port:", port)
 
 	//start web server
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
 	if err != nil {
 		log.Fatal(err)
 	}
