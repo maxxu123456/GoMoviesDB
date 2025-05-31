d1394cc0a29ea96039b91763c65bad1e587a7a79
Max Xu <maxxu123456@gmail.com>
2025-05-30 21:06:21 -0400

    added protected routes

--- cmd/api/auth.go
+++ cmd/api/auth.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/golang-jwt/jwt/v4"
@@ -112,4 +114,56 @@ func (j *Auth) GetExpiredRefreshCookie() *http.Cookie {
 		HttpOnly: true,
 		Secure:   true,
 	}
+}
+
+func (j *Auth) GetTokenFromHeaderAndVerify(w http.ResponseWriter, r *http.Request) (string, *Claims, error) {
+	w.Header().Add("Vary", "Authorization")
+
+	//get auth header
+	authHeader := r.Header.Get("Authorization")
+
+	//sanity check
+
+	if authHeader == "" {
+		return "", nil, errors.New("no auth header")
+	}
+
+	// split header on spaces
+	headerParse := strings.Split(authHeader, " ")
+	if len(headerParse) != 2 {
+		return "", nil, errors.New("invalid auth header")
+	}
+
+	// check to see if we have word Bearer
+	if headerParse[0] != "Bearer" {
+		return "", nil, errors.New("invalid auth header")
+	}
+
+	token := headerParse[1]
+
+	//declare empty claims
+
+	claims := &Claims{}
+
+	//parse token
+
+	_, err := jwt.ParseWithClaims(token, claims, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(j.Secret), nil
+	})
+	if err != nil {
+		//has prefix token has expired by
+		if strings.HasPrefix(err.Error(), "token is expired by") {
+			return "", nil, errors.New("expired token")
+		}
+		return "", nil, err
+	}
+
+	if claims.Issuer != j.Issuer {
+		return "", nil, errors.New("invalid issuer")
+	}
+
+	return token, claims, nil
 }
--- src/Components/ManageCatalogue.js
+++ src/Components/ManageCatalogue.js
@@ -1,9 +1,66 @@
+import { useEffect, useState } from "react";
+import { data, Link, useNavigate, useOutletContext } from "react-router-dom";
 
 const ManageCatalogue = () => {
+    const [movies, setMovies] = useState([])
+    const { jwtToken } = useOutletContext()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (jwtToken === "") {
+            navigate("/login")
+            return
+        }
+
+        const headers = new Headers()
+        headers.append("Content-Type", "application/json")
+        headers.append("Authorization", "Bearer " + jwtToken)
+
+        const requestOptions = {
+            method: "GET",
+            headers: headers,
+        }
+        fetch(`/admin/movies`, requestOptions)
+            .then((res) => res.json())
+            .then((data) => {
+                setMovies(data)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+
+    }, [jwtToken, navigate])
     return (
         <div className="text-center">
-            <h2>Manage Catalgoue</h2>
+            <h2>Manage Catalogue</h2>
             <hr />
+            <table className="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>Movie</th>
+                        <th>Release Date</th>
+                        <th>Rating</th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    {movies.map((movie) => {
+                        return <tr key={movie.id}>
+                            <td>
+                                <Link to={`/admin/movies/${movie.id}`}>
+                                    {movie.title}
+                                </Link>
+                            </td>
+                            <td>
+                                <Link>{movie.release_date}</Link>
+                            </td>
+                            <td>
+                                <Link>{movie.mpaa_rating}</Link>
+                            </td>
+                        </tr>
+                    })}
+                </tbody>
+            </table>
         </div>
 
     )
